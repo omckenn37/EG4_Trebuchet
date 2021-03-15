9d175b8b213e8b0743da8523cd22859df7dfb4c2
glenert41 <glenert41@charlottesvilleschools.org>
2021-03-15 17:15:27 +0000

    Flask Cleaned - should be able to start trebuchet flask thursday

--- Flask/trebuchetFlask/app.py
+++ Flask/trebuchetFlask/app.py
@@ -12,35 +12,25 @@ app = Flask(__name__)
 def index():
 	print(request.method)
 	if request.method == "POST":
-	        if request.form.get('button1') == 'button1':
+	       if request.form.get('button1') == 'button1':
                 	# pass
                		print("button1")
 
-		elif request.form.get('button2') == 'button2':
+	       elif request.form.get('button2') == 'button2':
                 	# pass # do something else
                 	print("button2")
 
-
-            	elif request.form.get('button3') == 'button3':
-                	# pass # do something else
-                	print("button3")
-
-
-            	elif  request.form.get('button4') == 'button4':
-                	# pass # do something else
-                	print("button4")
-
-
-            	else:
+	       else:
                 	# pass # unknown
                 	return render_template("index.html")
 
-        elif request.method == 'GET':
+	elif request.method == 'GET':
             # return render_template("index.html")
             print("No Post Back Call")
-        return render_template("index.html")
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=80)
+
 
