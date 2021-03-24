0c44517289e160703d5eac6ae5aaf18953d37c53
glenert41 <glenert41@charlottesvilleschools.org>
2021-03-24 16:36:26 +0000

    test

--- Flask/trebuchetFlask/app.py
+++ Flask/trebuchetFlask/app.py
@@ -77,8 +77,9 @@ def landed():
 
 	print("Landed Running")
 
-	plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+	plt.plot([1,2,3,4], [1,4,9,19], 'ro')
 	plt.axis([0, 6, 0, 20])
+	plt.savefig("MPLTest.png")
 	plt.show()
 
 
