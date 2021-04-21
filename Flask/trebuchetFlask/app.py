a4104d4b2eb6912ff0760fed4d779f37090efc01
glenert41 <glenert41@charlottesvilleschools.org>
2021-04-21 15:40:33 +0100

    get text image up and refreshing

--- Flask/trebuchetFlask/app.py
+++ Flask/trebuchetFlask/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+from PIL import Image, ImageDraw, ImageFont
 import RPi.GPIO as GPIO
+
+dataImageFilename = "dataImage.png"
+fnt = ImageFont.truetype('arial.ttf', 15)
+
 
 
 import random
@@ -36,92 +41,66 @@ RST = 24
 accelerometer = Adafruit_LSM303.LSM303() # accelerometer setup
 
 
-#Countdown Timer
-def runCountdownTimer():
-        for i in range (1,5):
-                num = 6 - i
-                print("Launching in " + str(num))
-                time.sleep(1)
-        print("Launching in 1")
-        time.sleep(1)
-        print("Countdown Done")
-
-
-#Actually Launch
-def Launch():
-
-	print("Launch Initiated")
-
-	# ADD THE ACTUAL LAUNCH CODE HERE
-
-	print("Launch Done")
-
-#Wind Up
-def windUp():
-
-	launchTrackingDelay = 1
-
-	print("Wind Up Running")
-	time.sleep(launchTrackingDelay)
-	print("Wind Up Done")
-
-def inFreefall():
-
-	print("Freefall Running")
-
-	#take readings
-
-	#if no longer in air
-	print("Freefall Done")
-
-def landed():
-
-	print("Landed Running")
-
-	colors = ['ro','mo', 'yo']
-	ax=plt.axes()
-	ax.set_facecolor("#000033")
-
-	plt.plot([1,2,3,4], [1,4,9,19], random.choice(colors))
-	plt.axis([0, 6, 0, 20])
-	plt.savefig("static/images/refreshTestImage.png")
-	plt.show()
-
-	print("Landed Done")
-
-
-
 @app.route("/", methods=["GET","POST"])
 def index():
 	print(request.method)
 	if request.method == "POST":
-	       if request.form.get('button1') == 'button1':
-                        #pass
+		if request.form.get('button1') == 'button1':
+			#pass
                         
-                        startPhrase = str("Lukas")
-                        startInput = input("Enter " + str(startPhrase) + "  to confirm Launch: ")
+			startPhrase = str("Lukas")
+			startInput = input("Enter " + str(startPhrase) + "  to confirm Launch: ")
 
-                        if startInput != startPhrase:
-                                 print("Invalid launch phrase, please type: " + str(startPhrase))                        
+			if startInput != startPhrase:
+				print("Invalid launch phrase, please type: " + str(startPhrase))                        
 
-                        if startInput == startPhrase:
-                                 runCountdownTimer()
-                                 Launch()
-                                 windUp()
-                                 inFreefall()
-                                 landed()
+			if startInput == startPhrase:
+				# runCountdownTimer()
+				for i in range (1,5):
+					num = 6 - i
+					print("Launching in " + str(num))
+					time.sleep(1)
+				print("Launching in 1")
+				time.sleep(1)
+				print("Countdown Done")
 
-	       else:
-                	# pass # unknown
-                	return render_template("index.html")
+				# Launch()
+				print("Launch Initiated")
+				#add actual launch code here
+				print("Launch Done")
+
+				# windUp()
+				print("Wind Up Started")
+				time.sleep(1)
+				print("Wind Up Done")
+
+				# inFreefall()
+				print("In Freefall")
+				#take readings
+				print("Freefall done")
+
+				# landed()
+				print("Landed Running")
+				colors = ['ro', 'mo', 'yo']
+				ax=plt.axes()
+				ax.set_facecolor("#000033")
+				plt.plot([1,2,3,4],[1,4,9,19], random.choice(colors))
+				plt.axis([0,6,0,20])
+				plt.savefig("static/images/refreshTestImage.png")
+				plt.show()
+				print("Landed Done")
+
+		else:
+			# pass # unknown
+			return render_template("index.html")
 
 	elif request.method == 'GET':
-            # return render_template("index.html")
-            print("No Post Back Call")
+		# return render_template("index.html")
+		print("No Post Back Call")
 	return render_template("index.html")
 
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=80)  #port was 80
+	app.run(host="0.0.0.0", port=80)  #port was 80
 
 
